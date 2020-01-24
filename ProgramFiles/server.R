f9d241f9dd1bbebf73a8bf210dc44b2b4db82b41
jack.VAIO <chen.j.shihchieh@gmail.com>
2020-01-23 18:04:35 -0800

    Front page with logo set up

--- ProgramFiles/server.R
+++ ProgramFiles/server.R
@@ -1,3 +1,5 @@
+#server.R
+
 #Loading in necessary packages and downloading them if necessary
 necessary_packages <- c("shiny")
 for(i in necessary_packages){
@@ -11,49 +13,23 @@ rm(i, necessary_packages)
 
 ####Server side for calculating and managing displays
 function(input, output){
-  
   ###The input the dynamic display
   output$main.display <- renderUI ({
     ## Front page -- A large logo for KPU.
     #insert Kwantlen logo
     list(
       #A button on the top right for some basic configurations: type of task, number of questions, Demographic Info or no. 
-      fluidRow(
-        list(column(4), column(4),
-          column(4, 
-                 actionButton("config", 
-                              img(src = file.path("icons", "gearicon_small.png"),
-                                               width = "20px", height = "20px")
-                              )
-                 )
-             ), offset = 5
-               ),
-      
-      
-      fluidRow(
-        column(1),
-        img(src = file.path("icons", "kpuLogo.jpg"), align = "center")
-        ),
-    
-    #A button on the bottom of the page for continue
-      fluidRow(
-        column(1),
-        actionButton("continue", h2("Start"), align = "center", width = "400px")
-        )
-    
+      fluidRow(actionButton("config", img(src = file.path("icons", "gearicon_small.png"), width = "20px", height = "20px", alt = "Configuration Button"))),
+      fluidRow(img(src = file.path("icons", "kpuLogo.jpg"), width = "100%", height = "25%", align = "center", alt = "KPU Logo"))
     )
-    
-    
+      
     ##Consent Form?
-    
     ##The stroop test itself
-    
     ##End of test message
-    
     ##Maybe the score
-    
     ##Option for closing the window
+      
   })
-  
+    
   
 }
--- ProgramFiles/ui.R
+++ ProgramFiles/ui.R
@@ -1,6 +1,9 @@
+#ui.R
 
 ####What participants see
-ui <- fluidPage(
+fluidPage(
   #A changing display based on a button input value
-  uiOutput('main.display')
+  uiOutput('main.display'),
+  #A button on the bottom of the page for starting the stroop test
+  actionButton(inputId = "start", label = h2("Start"), align = "center", width = "400px")
 )
